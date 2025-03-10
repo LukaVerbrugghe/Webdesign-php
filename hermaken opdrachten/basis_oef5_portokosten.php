50ee61f1f5ff4833f74f8afd2ff6b0881b1921bb
LukaVerbrugghe <lotrdetailsys@gmail.com>
2025-03-10 18:39:16 +0100

    Fix indent meneer (alweer)

--- hermaken opdrachten/basis_oef5_portokosten.php
+++ hermaken opdrachten/basis_oef5_portokosten.php
@@ -5,57 +5,57 @@
 <title>PHP basisoefeningen</title>
 <style type="text/css">
 	body {
-	color: #0A0A55;		
+		color: #0A0A55;		
 	}
 	#wrapper {
-width: 1080px;
-	margin-left: auto;
-	margin-right: auto;
-	border: solid 1px #0070C0;
-	padding: 0.3em;
+		width: 1080px;
+		margin-left: auto;
+		margin-right: auto;
+		border: solid 1px #0070C0;
+		padding: 0.3em;
 	}	
 	main {
-	min-height: 20em;
-	padding: 1em;
+		min-height: 20em;
+		padding: 1em;
 	}
 	footer {
-	height: 0.5em;
-	background-color: #0070C0;
+		height: 0.5em;
+		background-color: #0070C0;
 	}
     form {
-border: solid 1px #660066;
-background-color: #3CF;	
-font-family: Verdana, Geneva, sans-serif;
-font-size: 1.2em;
-margin-bottom: 2em;
-width: 100%;
-}
-form input, select {
-color: #C00;	
-font-size: 1.2em;
-}
+		border: solid 1px #660066;
+		background-color: #3CF;	
+		font-family: Verdana, Geneva, sans-serif;
+		font-size: 1.2em;
+		margin-bottom: 2em;
+		width: 100%;
+	}
+	form input, select {
+		color: #C00;	
+		font-size: 1.2em;
+	}
 
-strong {
-font-weight: 900;
-color: #C00;
-font-size: 1.3em;	
-}
-.onderlijn {
-border-bottom: solid 1px #C00;	
-}
-.rechts {
-text-align: right;
-border-bottom: solid 1px #C00;		
-}
-#links {
-float: left;
-width: 20%;
-margin-right: 3em;
-}
-#rechts {
-float: left;
-width: 20%;	
-}	
+	strong {
+		font-weight: 900;
+		color: #C00;
+		font-size: 1.3em;	
+	}
+	.onderlijn {
+		border-bottom: solid 1px #C00;	
+	}
+	.rechts {
+		text-align: right;
+		border-bottom: solid 1px #C00;		
+	}
+	#links {
+		float: left;
+		width: 20%;
+		margin-right: 3em;
+	}
+	#rechts {
+		float: left;
+		width: 20%;	
+	}	
 </style>
 </head>
 
