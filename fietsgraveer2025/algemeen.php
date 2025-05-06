c0a28abdd25b22c6a16b0a25ab7fe8efdf1402bc
Luka Verbrugghe <verbruggheluka0@gmail.com>
2025-05-06 10:10:28 +0200

    Inloggen

--- fietsgraveer2025/algemeen.php
+++ fietsgraveer2025/algemeen.php
@@ -1,4 +1,43 @@
 <?php
 session_start();
-$_SESSION['inlogstatus']="ingelogd";
+
+// in- en uitloggen
+// uitloggen
+if(isset($_POST['btnLogout'])){
+    $_SESSION['login'] = "";
+    $_SESSION['vnaam'] = "";
+    $_SESSION['fnaam'] = "";
+    $_SESSION['regid'] = "";
+}
+
+// inloggen
+if($_POST['btnLogin']=="Login"){ //mag ook isset zijn
+    $vnaam = $_POST['txtVnaam'];
+    $fnaam = $_POST['txtFnaam'];
+    $ww = $_POST['txtWW'];
+    $sqllogin = "SELECT * FROM tblregistratie WHERE voornaam='$vnaam' AND fnaam='$fnaam'";
+
+    $resultlogin = $db->query($sqllogin);
+    $rowlogin = $resultlogin->fetch_assoc();
+    if($resultlogin->num_rows==0){
+        // er zijn geen gebruikers met deze voor- en achternaam
+        $loginfout = "Gebruiker onbekend";
+    }
+    else{
+        // de gebruiker bestaat --> wachtwoord controleren
+        if($rowlogin['wachtwoord']==$ww){
+            //het wachtwoord is correct en de gebruiker mag ingelogd worden
+            $loginfout = "Gebruiker wordt ingelogd";
+            $_SESSION['login'] = "ingelogd";
+            $_SESSION['vnaam'] = "$vnaam";
+            $_SESSION['fnaam'] = "$fnaam";
+            $_SESSION['regid'] = $rowlogin['registratieID'];
+        }
+        else{
+            //het wachtwoord is niet correct
+            $loginfout = "Wachtwoord onjuist";
+        }
+    }
+}
+
 ?>
--- fietsgraveer2025/inc_aside.php
+++ fietsgraveer2025/inc_aside.php
@@ -1,14 +1,21 @@
 <form name="frmLogin" id="frmLogin" method="post">
+<?php
+    if($_SESSION['login']!="ingelogd"){
 
+        ?>
 <h4>Log hier op de website in</h4>
 <input type="text" name="txtVnaam" placeholder="Typ je voornaam in" class='inputsmall'>
 <input type="text" name="txtFnaam" placeholder="Typ je familienaam in" class='inputsmall'>
 <input type="password" name="txtWW" placeholder="Typ je wachtwoord in" class='inputsmall'>
 <p><input type="submit" name="btnLogin" value="Login"></p>
-
-<input type="submit" name="btnLogout" value="Logout XXX YYY">	
- 
-<p class='alert'>FEEDBACK/FOUTMELDINGEN</p>
+<?php
+}
+else{
+?>
+<input type="submit" name="btnLogout" value="Logout <?php echo $_SESSION['vnaam']." ".$_SESSION['fnaam'];?>">	
+ <?php
+ }?>
+<p class='alert'><?= $loginfout;?></p>
 </form>
 
 <img src='images/fietsster.jpg' class='img-fluid'>
