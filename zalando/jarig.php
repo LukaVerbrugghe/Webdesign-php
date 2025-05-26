44863e960754d6dd33bb48e25f879afb8bc1368c
LukaVerbrugghe <lotrdetailsys@gmail.com>
2025-05-26 17:38:36 +0200

    Jarige klanten (werkt niet)

--- zalando/jarig.php
+++ zalando/jarig.php
@@ -1,6 +1,21 @@
 <?php
 include("cnnConnection.php");
 include("algemeen.php");
+
+//jarige klanten ophalen
+$sqljarig = "SELECT Familienaam, Voornaam, Geboortedatum FROM tblklanten";
+$qryjarig = $db->query($sqljarig);
+
+while($rowjarig = $qryjarig->fetch_assoc()){
+    $Geboortedatum = strtotime($rowjarig['Geboortedatum']);
+    if(date('d',$Geboortedatum == date('d')) and date('m', $Geboortedatum) == date("m")){
+        //gebruiker is jarig
+        $vnaam = $rowjarig['Voornaam'];
+        $fnaam = $rowjarig["Familienaam"];
+        $leeftijd = date("Y") - date("Y", $Geboortedatum);
+        $output .= "<tr><td class='horlijn'>$vnaam $fnaam</td><td class='horlijn>$leeftijd jaar</td></tr>\n";
+    }
+}
 ?>
 <!doctype html>
 <html>
@@ -20,7 +35,9 @@ include("algemeen.php");
     <div id="content">
 <table>
 <tr><td class='horlijn'></td><td class='horlijn'></td></tr>
-</table>
+<?php
+echo $output;
+?></table>
 <p>Er zijn geen jarige klanten vandaag!</p>   
 	</div>
     <footer><?php include("footer.php");?></footer>
