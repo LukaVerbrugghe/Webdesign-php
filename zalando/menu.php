74cbefec9f29301563029be5b88cefccad2a3880
LukaVerbrugghe <lotrdetailsys@gmail.com>
2025-05-26 17:25:48 +0200

    Menu zalando

--- zalando/menu.php
+++ zalando/menu.php
@@ -1,3 +1,17 @@
 <ul>
-<li><a href='XXX'>YYY</a></li>
+<?php
+$sqlmenu = "SELECT * FROM tblmenu ORDER BY volgorde";
+$qrymenu = $db->query($sqlmenu);
+
+$output = ""; //niet vereist, maar onderdrukt warning
+
+while($rowmenu = $qrymenu->fetch_assoc()){
+    $ID = $rowmenu['menuID'];
+    $menutekst = $rowmenu['menutekst'];
+    $menulink = $rowmenu['menulink'];
+
+    $output .= "<li><a href='$menulink'>$menutekst</a></li>";
+}
+echo $output;
+?>
 </ul>
